eabf1e8ef9bd6bbad2683e03f623e0b530880b93
Gareth Slinn <gslinn@gamil.com>
2020-10-13 21:18:32 +0100

    add useStateStorage to repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { useStateStorage } from './hooks/useStateStorage';
 import './App.css';
 
 function App() {
+  const [value, setValue] =  useStateStorage('id');
+  const [inputValue, setInputValue] = useState('');
+  const handleInput = (e) => {
+    setInputValue(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{value}</h1>
+      <input type="text" onChange={handleInput}  />
+      <button onClick={() => setValue(inputValue)}>update</button>
     </div>
   );
 }
